ea7e9c57e510ea0677fb46aca225e88b3e0fd119
ydshieh <ydshieh@users.noreply.github.com>
2023-01-26 16:33:00 +0100

    temp

--- tests/test_configuration_common.py
+++ tests/test_configuration_common.py
@@ -211,7 +211,7 @@ class ConfigTester(object):
         attribute_used = False
         for attribute in attributes:
             for modeling_source in modeling_sources:
-                if f"config.{attribute}" in modeling_source or f'getattr(config, "{x}"' in modeling_source:
+                if f"config.{attribute}" in modeling_source or f'getattr(config, "{attribute}"' in modeling_source:
                     attribute_used = True
                     break
             if attribute_used:
